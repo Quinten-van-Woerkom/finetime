8a7e98e38ca8c3189c27a2eeaa82d2289f9a3f8d
Quinten E. van Woerkom <quintenvanwoerkom@gmail.com>
2025-07-16 20:19:20 +0200

    Added unit tests for TAI

--- src/time_scale/tai.rs
+++ src/time_scale/tai.rs
@@ -116,3 +116,80 @@ mod proof_harness {
         let _ = TaiTime::from_datetime(date, hour, minute, second);
     }
 }
+
+/// Verifies this implementation by computing the `TaiTime` for some known time stamps. To compute
+/// these known time stamps, we use the fact that TAI, just like Unix time, has days that are
+/// always exactly 86,400 seconds long. Hence, the differences are caused by only an offset in
+/// epoch, which is the difference between 1958 and 1970: 378691200 seconds.
+#[test]
+fn known_timestamps() {
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1958, Month::January, 1).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(0)
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1958, Month::January, 2).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(24 * 60 * 60),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1960, Month::January, 1).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(2 * 365 * 24 * 60 * 60),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1961, Month::January, 1).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new((3 * 365 + 1) * 24 * 60 * 60),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1970, Month::January, 1).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(378691200),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1976, Month::January, 1).unwrap(), 0, 0, 0)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(189302400 + 378691200),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(2025, Month::July, 16).unwrap(), 16, 23, 24)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(1752683004 + 378691200),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(2034, Month::December, 26).unwrap(), 8, 2, 37)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(2050732957 + 378691200),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(2760, Month::April, 1).unwrap(), 21, 59, 58)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(24937883998 + 378691200),
+    );
+
+    assert_eq!(
+        TaiTime::from_datetime(Date::new(1643, Month::January, 4).unwrap(), 1, 1, 33)
+            .unwrap()
+            .elapsed_time_since_epoch(),
+        Seconds::new(-10318834707 + 378691200),
+    );
+}
