9edb98f55616d9d5cdace7c4cae98c03937cbbff
Quinten E. van Woerkom <quintenvanwoerkom@gmail.com>
2025-07-29 20:45:01 +0200

    Added placeholder `build.rs` contents to be used when deploying to `docs.rs`

--- build.rs
+++ build.rs
@@ -4,6 +4,7 @@
 use std::fs;
 use std::path::PathBuf;
 
+#[cfg(not(docsrs))]
 fn main() {
     // Step 1: Download from the current IANA URL
     const URL: &str = "https://data.iana.org/time-zones/data/leap-seconds.list";
@@ -63,6 +64,62 @@ fn main() {
     // Step 4: Write to build output directory
     let out = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("leap_seconds.rs");
     fs::write(&out, output).expect("Failed to write leap_seconds.rs");
+
+    println!("cargo:rerun-if-changed=build.rs");
+}
+
+/// Placeholder build function that is called when running on `docsrs`. Does not require internet
+/// to build, but might contain an out-of-date leap seconds table.
+#[cfg(docsrs)]
+fn main() {
+    const PLACEHOLDER_CONTENTS: &str = r"
+// Automatically-generated leap table based on the `leap-seconds.list` published by IANA at
+// https://data.iana.org/time-zones/data/leap-seconds.list
+// Formatted as (Unix timestamp of UTC second, total TAI-UTC seconds)
+// 
+// WARNING: This version was generated without internet, so might be out-of-date.
+
+use lazy_static::lazy_static;
+use crate::duration::Seconds;
+
+lazy_static! {
+pub(crate) static ref LEAP_SECONDS: LeapSecondsTable = {
+    let mut leap_seconds = LeapSecondsTable::default();
+    leap_seconds.insert(Seconds::new(63072000), Seconds::new(0));
+    leap_seconds.insert(Seconds::new(78796800), Seconds::new(1));
+    leap_seconds.insert(Seconds::new(94694400), Seconds::new(2));
+    leap_seconds.insert(Seconds::new(126230400), Seconds::new(3));
+    leap_seconds.insert(Seconds::new(157766400), Seconds::new(4));
+    leap_seconds.insert(Seconds::new(189302400), Seconds::new(5));
+    leap_seconds.insert(Seconds::new(220924800), Seconds::new(6));
+    leap_seconds.insert(Seconds::new(252460800), Seconds::new(7));
+    leap_seconds.insert(Seconds::new(283996800), Seconds::new(8));
+    leap_seconds.insert(Seconds::new(315532800), Seconds::new(9));
+    leap_seconds.insert(Seconds::new(362793600), Seconds::new(10));
+    leap_seconds.insert(Seconds::new(394329600), Seconds::new(11));
+    leap_seconds.insert(Seconds::new(425865600), Seconds::new(12));
+    leap_seconds.insert(Seconds::new(489024000), Seconds::new(13));
+    leap_seconds.insert(Seconds::new(567993600), Seconds::new(14));
+    leap_seconds.insert(Seconds::new(631152000), Seconds::new(15));
+    leap_seconds.insert(Seconds::new(662688000), Seconds::new(16));
+    leap_seconds.insert(Seconds::new(709948800), Seconds::new(17));
+    leap_seconds.insert(Seconds::new(741484800), Seconds::new(18));
+    leap_seconds.insert(Seconds::new(773020800), Seconds::new(19));
+    leap_seconds.insert(Seconds::new(820454400), Seconds::new(20));
+    leap_seconds.insert(Seconds::new(867715200), Seconds::new(21));
+    leap_seconds.insert(Seconds::new(915148800), Seconds::new(22));
+    leap_seconds.insert(Seconds::new(1136073600), Seconds::new(23));
+    leap_seconds.insert(Seconds::new(1230768000), Seconds::new(24));
+    leap_seconds.insert(Seconds::new(1341100800), Seconds::new(25));
+    leap_seconds.insert(Seconds::new(1435708800), Seconds::new(26));
+    leap_seconds.insert(Seconds::new(1483228800), Seconds::new(27));
+    leap_seconds
+};
+}
+";
+
+    let out = PathBuf::from(std::env::var("OUT_DIR").unwrap()).join("leap_seconds.rs");
+    fs::write(&out, PLACEHOLDER_CONTENTS).expect("Failed to write leap_seconds.rs");
 
     println!("cargo:rerun-if-changed=build.rs");
 }
